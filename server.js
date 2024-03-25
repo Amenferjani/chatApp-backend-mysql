7374aaa12153aed6626068ec1ba76cc72942cbeb
amenferjani <129557510+Amenferjani@users.noreply.github.com>
2024-03-25 22:39:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const app = require("./app");
 const server = http.createServer(app);
 const io = socketIO(server);
 
-//!! ... (Configure and set up your Express app)
-
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
-
-//     //!!  Handle socket events here
-//     socket.on('disconnect', () => {
-//         console.log('A user disconnected');
-//     });
-// });
 
 server.listen(3000, () => {
     console.log('starting the server.....');
